44ab9ac3a545033a16d53729fce20b1cd1d58933
NarendraMalle <narendra.leander22@gmail.com>
2024-03-18 21:09:43 -0400

    color added

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def predict():
     features = [float(x) for x in request.form.values()]
     final_features = [np.array(features)]
     prediction = model.predict(final_features)
-    return render_template('index.html', prediction_text='Predicted Weight: {}'.format(prediction[0]))
+    return render_template('result.html', prediction_text='Predicted Weight: {}'.format(prediction[0]))
 
 if __name__ == "__main__":
     app.run(debug=True)
